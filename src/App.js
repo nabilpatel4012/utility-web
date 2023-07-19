817208118cae4444d8482bbb9e477d5afe1d85f7
nabilpatel4012 <nabilpatel4012@gmail.com>
2023-07-19 12:13:59 +0530

    removed unused files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
-import RecipeReviewCard from './components/dashboard/main-card';
 import CardWithButton from './components/dashboard/new_card';
 import BillCard from './components/dashboard/third_card';
-import BasicButtons from './components/dashboard/buttons';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       <div style={{marginLeft: '20px'}}>
+        <CardWithButton></CardWithButton>
       <BillCard></BillCard>
       </div>
       
